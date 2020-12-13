55eeb5316abbeba0da70b21ecd52b59b897c4034
ChenyangLEI <935747833@qq.com>
2020-12-13 14:45:10 +0800

    fix a bug

--- loss.py
+++ loss.py
@@ -2,7 +2,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-import os,time,cv2,
+import os
+import time
+import cv2
 from scipy import io
 import tensorflow as tf
 import utils as utils
